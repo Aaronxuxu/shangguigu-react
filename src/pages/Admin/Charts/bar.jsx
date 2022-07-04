c08e6f95eb4613bfcd72926a0fba82b81e720a5a
unknown <470164479@qq.com>
2022-07-04 13:55:49 +0800

    更新canvas

--- src/pages/Admin/Charts/bar.jsx
+++ src/pages/Admin/Charts/bar.jsx
@@ -1,6 +1,41 @@
 // 柱形图路由
-import React from "react";
+import React, { useEffect } from "react";
+import { Card, Button } from "antd";
+import ReactECharts from "echarts-for-react";
+const UpdateTitle = () => {
+  return <Button type="primary">更新</Button>;
+};
 const Bar = () => {
-  return <div></div>;
+  const option = {
+    tooltip: {},
+    legend: {
+      data: ["销量"],
+    },
+    xAxis: {
+      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "销量",
+        type: "bar",
+        data: [5, 20, 36, 10, 10, 20],
+      },
+    ],
+  };
+  useEffect(() => {
+    console.log();
+  }, []);
+  return (
+    <Card title={<UpdateTitle />}>
+      <Card title="柱状图一">
+        <ReactECharts
+          option={option}
+          style={{ height: 400 }}
+          opts={{ renderer: "svg" }}
+        />
+      </Card>
+    </Card>
+  );
 };
 export default Bar;
--- src/pages/Admin/Charts/line.jsx
+++ src/pages/Admin/Charts/line.jsx
@@ -1,6 +1,54 @@
 //  线形路由
-import React from "react";
+import React, { useEffect } from "react";
+import { Card, Button } from "antd";
+import ReactECharts from "echarts-for-react";
+import "echarts/i18n/langFR";
+
+const UpdateTitle = () => {
+  return <Button type="primary">更新</Button>;
+};
 const Line = () => {
-  return <div>Line</div>;
+  const option = {
+    title: {
+      text: "ECharts 入门示例",
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+        dataZoom: {},
+        restore: {},
+      },
+    },
+    tooltip: {},
+    legend: {
+      data: ["销量"],
+    },
+    xAxis: {
+      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "销量",
+        type: "line",
+        data: [5, 20, 36, 10, 10, 20],
+      },
+    ],
+  };
+  useEffect(() => {
+    console.log();
+  }, []);
+  return (
+    <Card title={<UpdateTitle />}>
+      <Card title="柱状图一">
+        <ReactECharts
+          option={option}
+          style={{ height: 400 }}
+          opts={{ locale: "FR" }}
+        />
+        ;
+      </Card>
+    </Card>
+  );
 };
 export default Line;
